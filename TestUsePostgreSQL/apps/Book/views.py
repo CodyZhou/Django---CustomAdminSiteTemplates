a20476187831b74024e561ebc3932834ffe6e1d8
HappyCody <happycodyinla@gmail.com>
2017-03-06 17:20:34 -0800

    03/06/2017 This is a test message from local

--- TestUsePostgreSQL/apps/Book/views.py
+++ TestUsePostgreSQL/apps/Book/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 # Test a Book app.
 # I just find a error and fixed it!
+# I got this message. So, from now on, we can use the same project at home and office.
+# It is awesome!
