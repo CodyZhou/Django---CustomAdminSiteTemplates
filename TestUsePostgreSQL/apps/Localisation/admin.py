c48bfa439e3d1c05d2f99e570b3578a731429e8c
HappyCody <happycodyinla@gmail.com>
2017-03-14 13:12:24 -0700

    03/08/2017  Normal Updated

--- TestUsePostgreSQL/apps/Localisation/admin.py
+++ TestUsePostgreSQL/apps/Localisation/admin.py
@@ -1,9 +1,40 @@
 from django.contrib import admin
 
+from TestUsePostgreSQL.libs.admin.links import AdminSpecialLinks
 from TestUsePostgreSQL.apps.Localisation.models import Country, Zone
 
 
 # Register your models here.
-admin.site.register(Country)
-admin.site.register(Zone)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'iso_code_3', 'status', 'edit_link')
+    list_display_links = ('edit_link',)
+    list_filter = ('status',)
+
+    search_fields = ('name', 'iso_code_3')
+
+    def edit_link(self, country):
+        return AdminSpecialLinks().edit_link
+    edit_link.short_description = 'Action'
+
+
+admin.site.register(Country, CountryAdmin)
+
+
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'show_country_name', 'status', 'edit_link')
+    list_display_links = ('edit_link',)
+    list_filter = ('status',)
+
+    search_fields = ('name', 'country__name')
+
+    def show_country_name(self, zone):
+        return zone.country.name
+    show_country_name.short_description = "Country Name"
+
+    def edit_link(self, zone):
+        return AdminSpecialLinks().edit_link
+    edit_link.short_description = 'Action'
+
+
+admin.site.register(Zone, ZoneAdmin)
 
--- TestUsePostgreSQL/apps/Publisher/admin.py
+++ TestUsePostgreSQL/apps/Publisher/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
+from TestUsePostgreSQL.libs.admin.links import AdminSpecialLinks
 from TestUsePostgreSQL.apps.Publisher.models import Publisher
 
 
 # Register your models here.
-admin.site.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'added_date', 'edit_link')
+    list_display_links = ('edit_link',)
+    search_fields = ('name',)
+
+    def edit_link(self, publisher):
+        return AdminSpecialLinks().edit_link
+    edit_link.short_description = 'Action'
+
+admin.site.register(Publisher, PublisherAdmin)
 
