d337755f932606aa57f1f583dc814be0f9b3adfc
HappyCody <happycodyinla@gmail.com>
2017-03-21 19:40:31 -0700

    Change file format!

--- TestUsePostgreSQL/apps/Author/admin.py
+++ TestUsePostgreSQL/apps/Author/admin.py
@@ -33,7 +33,7 @@ class AuthorAdmin(admin.ModelAdmin):
     pass
 
 # Register your models here.
-#admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Author, AuthorAdmin)
 
 
 @admin.register(AuthorAddress)
@@ -60,5 +60,5 @@ class AuthorAddressAdmin(admin.ModelAdmin):
 
     pass
 
-#admin.site.register(AuthorAddress, AuthorAddressAdmin)
+# admin.site.register(AuthorAddress, AuthorAddressAdmin)
 
